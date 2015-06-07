55abc5e6e5ce110d58d3e5901f05aa9b00b6527e
Brookelyn <brooke.balza@gmail.com>
2015-06-07 15:33:07 +0000

    Updating yet again

--- app/scripts/album.js
+++ app/scripts/album.js
@@ -1,4 +1,37 @@
-var changeAlbumView = function(){
+// Example Album
+var albumPicasso = {
+    name: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: '/images/album-placeholder.png',
+    songs: [
+        { name: 'Blue', length: '4:26' },
+        { name: 'Green', length: '3:14' },
+        { name: 'Red', length: '5:01' },
+        { name: 'Pink', length: '3:21'},
+        { name: 'Magenta', length: '2:15'}
+    ]
+};
+ 
+// Another Example Album
+var albumMarconi = {
+  name: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: '/images/album-placeholder.png',
+    songs: [
+        { name: 'Hello, Operator?', length: '1:01' },
+        { name: 'Ring, ring, ring', length: '5:01' },
+        { name: 'Fits in your pocket', length: '3:21'},
+        { name: 'Can you hear me now?', length: '3:14' },
+        { name: 'Wrong phone number', length: '2:15'}
+    ]
+};
+
+
+var changeAlbumView = function(album){
 
 	// Update the album title
 	var $albumTitle = $('.album-title');
@@ -41,37 +74,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 
 
 
-// Example Album
-var albumPicasso = {
-   	name: 'The Colors',
-   	artist: 'Pablo Picasso',
-   	label: 'Cubism',
-   	year: '1881',
-   	albumArtUrl: '/images/album-placeholder.png',
-   	songs: [
-       	{ name: 'Blue', length: '4:26' },
-       	{ name: 'Green', length: '3:14' },
-       	{ name: 'Red', length: '5:01' },
-       	{ name: 'Pink', length: '3:21'},
-       	{ name: 'Magenta', length: '2:15'}
-    ]
-};
- 
-// Another Example Album
-var albumMarconi = {
-	name: 'The Telephone',
-   	artist: 'Guglielmo Marconi',
-   	label: 'EM',
-   	year: '1909',
-   	albumArtUrl: '/images/album-placeholder.png',
-   	songs: [
-       	{ name: 'Hello, Operator?', length: '1:01' },
-       	{ name: 'Ring, ring, ring', length: '5:01' },
-       	{ name: 'Fits in your pocket', length: '3:21'},
-       	{ name: 'Can you hear me now?', length: '3:14' },
-       	{ name: 'Wrong phone number', length: '2:15'}
-    ]
-};
+
 
 
 
@@ -83,5 +86,3 @@ if (document.URL.match(/\/album.html/)) {
 
 	});
 }
-
-Testing blarg
