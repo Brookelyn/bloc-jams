6c11e7206edeb1eb0754ff18e7ddc32e9ce36dbe
Brookelyn <brooke.balza@gmail.com>
2015-06-18 11:28:02 +0000

    Converted collection view to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,20 @@
-// require('./landing');
-// require('./collection');
-// require('./album');
-// require('./profile');
+// Example Album
+var albumPicasso = {
+    name: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: '/images/album-placeholder.png',
+    songs: [
+        { name: 'Blue', length: '4:26' },
+        { name: 'Green', length: '3:14' },
+        { name: 'Red', length: '5:01' },
+        { name: 'Pink', length: '3:21'},
+        { name: 'Magenta', length: '2:15'}
+    ]
+};
+
+
 
 blocJams = angular.module('BlocJams', ['ui.router']);
     blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
@@ -12,9 +25,18 @@ blocJams = angular.module('BlocJams', ['ui.router']);
         controller: 'Landing.controller',
         templateUrl: '/templates/landing.html'
     });
+
+    $stateProvider.state('collection', {
+        url: '/collection',
+        controller: 'Collection.controller',
+        templateUrl: '/templates/collection.html'
+    });
+
 }]);
+
+
  
-// This is a cleaner way to call the controller than crowding it on the module definition.
+// Landing controller
 blocJams.controller('Landing.controller', ['$scope', function($scope) {
 	
     $scope.headText = "Bloc Jams";
@@ -45,6 +67,17 @@ blocJams.controller('Landing.controller', ['$scope', function($scope) {
      	'/images/album-placeholders/album-7.jpg',
      	'/images/album-placeholders/album-8.jpg',
      	'/images/album-placeholders/album-9.jpg',
-   ];
+    ];
 
+}]);
+
+
+
+// Collection controller
+blocJams.controller('Collection.controller', ['$scope', function($scope) {
+    $scope.albums = [];
+
+    for (var i = 0; i < 33; i++) {
+        $scope.albums.push(angular.copy(albumPicasso));
+    }
 }]);
